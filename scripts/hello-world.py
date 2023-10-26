190045e754554361e10eb8a338c0afde85931160
Cristian Palma <cristianpalma1703@gmail.com>
2023-10-26 15:56:20 -0600

    wip

--- scripts/hello-world.py
+++ scripts/hello-world.py
@@ -1,5 +1,11 @@
 print("Hola Lil Josue;")
-
+from pyspark.sql import SparkSession
 spark=SparkSession.builder.getOrCreate()
-df=spark.read.options(header='True',inferShema='True').csv(/resources/Data/*.csv)
-df.count()
+df=spark.read.options(header='True',inferShema='True').csv('/resources/Data/*.csv')
+
+
+products.mode.write('overwrite').csv("/resultado/result1")
+
+producto = df.select(['product_id']).filter("event_type='cart'").first()
+
+
