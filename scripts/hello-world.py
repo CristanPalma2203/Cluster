728213e59083ec1d8be3589508d9d4f1186b4820
Cristian Palma <cristianpalma1703@gmail.com>
2023-10-26 19:40:07 -0600

    wip

--- scripts/hello-world.py
+++ scripts/hello-world.py
@@ -1,11 +1,10 @@
-print("Hola Lil Josue;")
 from pyspark.sql import SparkSession
 spark=SparkSession.builder.getOrCreate()
-df=spark.read.options(header='True',inferShema='True').csv('/resources/Data/*.csv')
 
+df=spark.read.options(header='True',inferShema='True').csv('/resources/Data/*.csv')
 
 products.mode.write('overwrite').csv("/resultado/result1")
 
 producto = df.select(['product_id']).filter("event_type='cart'").first()
 
-
+sessiones = df.select(['user_session']).filter("event_type='purchase' and product_id = "+producto).distinc()
